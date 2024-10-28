98041a374ae40ae097436ce1ea62a41bd4adfa2e
osariusz <osariusz@interia.pl>
2024-10-28 12:54:23 +0100

    Simple backend for plots

--- src/Plotting.py
+++ src/Plotting.py
@@ -1,10 +1,13 @@
 from ChannelAnalysis import ChannelAnalysis
 import matplotlib.pyplot as plt
 import pandas as pd
+from io import BytesIO
 
 class Plotting():
 
-    def plot_messages_weekday(self, channel_analysis: ChannelAnalysis):
+    TEMP_PLOT_NAME: str = "TEMP_PLOT.png"
+
+    def plot_messages_weekday(self, channel_analysis: ChannelAnalysis) -> str:
         df: pd.DataFrame = channel_analysis.weekday_number_of_messages().copy()
         day_dict = {
             0: 'Monday',
@@ -17,4 +20,9 @@ class Plotting():
         }
         df["weekday_name"] = df.index.to_series().replace(day_dict)
         df.plot(x="weekday_name", xlabel="Weekday", y="content", ylabel="Messages", title="Sent messages by weekday")
-        plt.show()
+
+        plot_filename = self.TEMP_PLOT_NAME
+        plt.savefig(plot_filename)
+        plt.close()
+
+        return plot_filename
--- src/analysis_main.py
+++ src/analysis_main.py
@@ -1,7 +1,21 @@
 from ChannelAnalysis import ChannelAnalysis
 from Plotting import Plotting
+from fastapi import FastAPI, responses
+from fastapi.responses import StreamingResponse
+import matplotlib.pyplot as plt
+import numpy as np
+from io import BytesIO
+from flask import Flask, send_file
 
-if(__name__ == "__main__"):
+app = FastAPI()
+
+@app.get('/plot')
+async def get_plot():
     channel_analysis: ChannelAnalysis = ChannelAnalysis()
     plotting: Plotting = Plotting()
-    plotting.plot_messages_weekday(channel_analysis)
+    plot_file: str = plotting.plot_messages_weekday(channel_analysis)
+    return responses.FileResponse(plot_file)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)

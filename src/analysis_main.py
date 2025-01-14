2983a2015e464f080b7ddeb77434aedf26b88a90
osariusz <osariusz@interia.pl>
2025-01-14 17:14:20 +0100

    Change preparation

--- src/analysis_main.py
+++ src/analysis_main.py
@@ -9,20 +9,28 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = FastAPI()
+main_channel_analysis: ChannelAnalysis
 
 @app.get('/plot')
 async def get_plot():
     channels = []
-    main_channel_analysis: ChannelAnalysis = ChannelAnalysis()
     all_channel_analysis: list[ChannelAnalysis] = []
     for channel in channels:
         channel_analysis: ChannelAnalysis = ChannelAnalysis(message_df=main_channel_analysis.messages_df)
         channel_analysis.restrict_to_channels([channel])
         all_channel_analysis.append(channel_analysis)
     plotting: Plotting = Plotting()
+    plot_file: str = plotting.plot_messages_percent_weekday(all_channel_analysis)
+    return responses.FileResponse(plot_file)
+
+@app.get('/plot_all')
+async def get_plot():
+    plotting: Plotting = Plotting()
     plot_file: str = plotting.plot_messages_percent_weekday([main_channel_analysis])
     return responses.FileResponse(plot_file)
 
 if __name__ == "__main__":
+    logging.info("Preparing data")
+    main_channel_analysis = ChannelAnalysis()
     logging.info("Starting the analysis")
     uvicorn.run(app, host="0.0.0.0", port=8000)
